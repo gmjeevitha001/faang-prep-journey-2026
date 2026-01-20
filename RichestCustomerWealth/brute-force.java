20c9a0971362c0e39f26bc1d6c5a7502716da5ec
Jeevitha <gmjeevitha001@gmail.com>
2026-01-20 23:40:25 +0530

    Create brute-force.java

---
+++ RichestCustomerWealth/brute-force.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int maxWealth = 0;
+
+        // Outer loop: each customer
+        for (int i = 0; i < accounts.length; i++) {
+            int sum = 0; // reset for each customer
+
+            // Inner loop: sum all accounts of this customer
+            for (int j = 0; j < accounts[i].length; j++) {
+                sum += accounts[i][j];
+            }
+
+            // Track the richest
+            if (sum > maxWealth) {
+                maxWealth = sum;
+            }
+        }
+
+        return maxWealth;
+    }
+}
